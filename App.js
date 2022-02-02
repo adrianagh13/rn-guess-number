c6d1da87954c79b9ef0e708f3e104897ee2da9ec
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2022-02-01 18:33:29 -0600

    Renderization of screens modified

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {useState} from 'react/cjs/react.development';
 import Header from './components/Header';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+  //in RN we can store components in variables, so when we render the variable
+  //in the return section like this {content} the components get rendered
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
   return (
     <View style={styles.screen}>
       <Header title="Guess a number" />
-      <StartGameScreen />
-      <GameScreen />
+      {content}
     </View>
   );
 }
